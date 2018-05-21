6a01b4d46652981f08870f2e7110b308d1bde37e
Yuan bai <bybai@cn.ibm.com>
2018-05-21 05:35:58 -0400

    configure policy table

--- HA/xcatha-setup.py
+++ HA/xcatha-setup.py
@@ -5,6 +5,7 @@
 
 import argparse
 import os
+import time
 import pdb
 
 def log_info(message):
@@ -13,20 +14,24 @@ def log_info(message):
 
 class xcat_ha_utils:
 
+    def log_info(self, message):
+        print "================================================================="
+        print message
+
     def vip_check(self, vip):
         """check if virtual ip can ping or not"""
-        log_info("ping virtual ip ... ...")
+        self.log_info("ping virtual ip ... ...")
         pingcmd="ping -c 1 -w 10 "+vip
         res=os.system(pingcmd)
         if res is 0:
             message="Error: Aborted startup as virtual ip appears to be already active."
-            log_info(message)
+            self.log_info(message)
             exit(1)
         else:
             message="virtual ip can be used."
-            log_info(message) 
+            self.log_info(message) 
 
-    def execute_service(self, cmd):
+    def execute_command(self, cmd):
         """"""
         loginfo="Running command:"+cmd
         print loginfo
@@ -36,8 +41,8 @@ class xcat_ha_utils:
                 loginfo=cmd+" [Failed]"
                 print loginfo
                 return 1
-            else a >0:
-                sleep 3
+            if a >0:
+                time.sleep(3)
                 loginfo="Retry "+a+" ... ..."+cmd
                 print loginfo
                 res=os.system(cmd)
@@ -49,21 +54,19 @@ class xcat_ha_utils:
 
     def configure_xcat_attribute(self, host, ip):
         "configure xcat MN attribute"
-        log_info("Configure xCAT management node attribute")
-        
-
+        self.log_info("Configure xCAT management node attribute")
         
     def configure_vip(self, vip, nic, mask):
         """configure virtual ip"""
-        log_info("Start configure virtual ip as alias ip")
+        self.log_info("Start configure virtual ip as alias ip")
         cmd="ifconfig "+nic+" "+vip+" "+" netmask "+mask
         res=os.system(cmd)
         if res is 0:
             message="configure virtual IP [passed]."
-            log_info(message)
+            self.log_info(message)
         else :
             message="Error: configure virtual IP [failed]."
-            log_info(message) 
+            self.log_info(message) 
             exit(1)
         #add virtual ip into /etc/resolve.conf
         resolvefile=open('/etc/resolve.conf','a')
@@ -73,7 +76,7 @@ class xcat_ha_utils:
  
     def change_hostname(self, host, ip):
         """change hostname"""
-        log_info("Start configure hostname")
+        self.log_info("Start configure hostname")
         hostfile=open('/etc/hosts','a')
         ip_and_host=ip+" "+host
         hostfile.write(ip_and_host)
@@ -81,7 +84,7 @@ class xcat_ha_utils:
 
     def unconfigure_vip(self, vip, nic):
         """remove vip from nic and /etc/resolve.conf"""
-        log_info("remove virtual ip")
+        self.log_info("remove virtual ip")
         cmd="ifconfig "+nic+" 0.0.0.0 0.0.0.0"
         print cmd
         res=os.system(cmd)
@@ -90,10 +93,10 @@ class xcat_ha_utils:
         res=os.system(cmd)
         if res is 0:
             message="remove virtual IP [passed]."
-            log_info(message)
+            self.log_info(message)
         else :
             message="Error: fail to remove virtual IP [failed]."
-            log_info(message)
+            self.log_info(message)
             exit(1)
 
     def check_service_status(service_name):
@@ -101,11 +104,50 @@ class xcat_ha_utils:
         status = os.system('systemctl status '+service_name+ ' > /dev/null')
         return status
 
-        
-    def check_xcat_attribute(self, vip):
-        """check master and nameservers in site table, tftpserver in networks table"""
-        pass      
+    def finditem(n,server):
+        """add item into policy table"""
+        index=bytes(n)
+        cmd="lsdef -t policy |grep 1."+index
+        res=os.system(cmd)
+        if res is not 0:
+            cmd="chdef -t policy 1."+n+" name="+server+" rule=trusted"
+            res=os.system(cmd)
+            if res is 0:
+                loginfo=cmd+" [Passed]"
+                print loginfo
+                return 0
+            else:
+                loginfo=cmd+" [Failed]"
+                return 1
+        else:
+            n+=1
+            finditem(bytes(n),server)
 
+    def change_xcat_policy_attribute(self, nic, vip):
+        """add hostname into policy table"""
+        self.log_info("Configure xCAT policy table")
+        filename="/etc/xcat/cert/server-cert.pem"
+        word="Subject: CN="
+        server=""
+        with open(filename, 'r') as f:
+            for l in f.readlines():
+                if word in l:
+                    linelist=l.split("=")
+                    server=linelist[1].strip()
+                    break
+        if server:
+            cmd="lsdef -t policy -i name|grep "+server
+            res=os.system(cmd)
+            if res is not 0:
+                res=finditem(3,server)
+                if res is 0:
+                    return 0
+            else:
+                loginfo=server+" exist in policy table."
+                return 0
+        else:
+            loginfo="Error: get server name "+server+" failed." 
+        return 1       
 
     def copy_files(sourceDir, targetDir):  
         print sourceDir  
@@ -129,9 +171,9 @@ class xcat_ha_utils:
                 copy_files(sourceF, targetF)
 
 
-    def configure_shared_data(self, path, sharedfs)
+    def configure_shared_data(self, path, sharedfs):
         """configure shared data directory"""
-        log_info("configure shared data directory")
+        self.log_info("configure shared data directory")
         #check if there is xcat data in shared data directory
         xcat_file_path=path+"/etc/xcat"
         if not os.path.exists(xcat_file_path):
@@ -143,12 +185,12 @@ class xcat_ha_utils:
                     os.mkdir(xcat_file_path)
                     copy_files(sharedfs[i],xcat_file_path)
                     i += 1  
-       #create symlink 
-       i=0
-       while i < len(sharedfs):
-           print "create symlink ..."
-           xcat_file_path=path+sharedfs[i]
-           os.symlink(xcat_file_path, sharedfs[i]) 
+        #create symlink 
+        i=0
+        while i < len(sharedfs):
+            print "create symlink ..."
+            xcat_file_path=path+sharedfs[i]
+            os.symlink(xcat_file_path, sharedfs[i]) 
 
 def parser_arguments():
     """parser input arguments"""
